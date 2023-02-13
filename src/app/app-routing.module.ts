9b0b41e0bba9e401edb6884b4bdc3273378d04aa
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2023-02-13 01:26:50 +0100

    chore(app): se agrega routeo a modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pricing', loadChildren: () => import('./modules/pricing/pricing.module').then((m) => m.PricingModule) },
+  { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then((m) => m.ContactModule) },
+  { path: 'table', loadChildren: () => import('./modules/customer-table/customer-table.module').then((m) => m.CustomerTableModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
